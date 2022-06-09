479c77c96ad188c9ca7bb88d83bcdc330aed3acb
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-06-10 01:34:51 +0530

    just undertanding

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParse = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,6 +12,13 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+//connecting mongoose to mongodb
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//handeling main page
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
@@ -18,6 +27,12 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+//handeling undefined routes
+app.get("*", (req, res) => {
+  res.write("Not Found");
+  res.end();
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
